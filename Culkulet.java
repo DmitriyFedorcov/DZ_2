ac76669b466f6d948c648e6983c16f78d03b0946
DmitriyFedorcov <49728683+DmitriyFedorcov@users.noreply.github.com>
2019-04-17 20:16:42 +0300

    Add files via upload

---
+++ Culkulet.java
@@ -0,0 +1,103 @@
+package Lessons_2;
+
+import java.util.Scanner;
+
+public class Culkulet {
+//    public void fibo(){
+//        System.out.println("Числа фибоначи: ");
+//        int k = 1;
+//        int n = 1;
+//        int b;
+//        for (int z = 1; z<=13;z++){
+//            b = k + n;
+//            System.out.println( b);
+//            k = n;
+//            n = b;
+//        }
+//        System.out.println();
+//        }
+
+    //    public void tabl() {
+//        System.out.println("Таблица умножения: ");
+//        for (int z = 1; z <= 10; z++) {
+//            System.out.println();
+//            for (int k = 1; k <= 10; k++) {
+//                int t = z * k;
+//        System.out.printf("%8d", t);
+//        }
+//        }
+//        System.out.println();
+//        }
+//    public void elka1() {
+//        for (int k = 1; k < 10; k++) {
+//            for (int n = 1; n < k; n++) {
+//                System.out.print("* ");
+//
+//            }
+//            System.out.println();
+//        }
+//    }
+
+//    public void elka2() {
+//        for (int k = 10; k > 0; k--) {
+//            for (int n = 1; n < k; n++) {
+//                System.out.print("* ");
+//
+//            }
+//            System.out.println();
+//        }
+//    }
+
+    //    public void vremya(int hour) {
+//        if ((hour >= 22 && hour <= 24) || (hour >= 0 && hour <= 3)) {
+//            System.out.println("Доброй ночи");
+//
+//
+//        } else if ((hour >= 4 && hour <= 24) || (hour >= 0 && hour <= 11)) {
+//            System.out.println("Доброе утро");
+//
+//        } else if ((hour >= 12 && hour <= 24) || (hour >= 0 && hour <= 18)) {
+//            System.out.println("Добрый день");
+//
+//        } else if ((hour >= 19 && hour <= 24) || (hour >= 0 && hour <= 21)) {
+//            System.out.println("Добрый вечер");
+//
+//        } else {
+//            System.out.println("Извените такого времени нет");
+//
+//        }
+//    }
+//    public void bilet() {
+//        System.out.println("Лотерейная игра выйграй ничего угадав числа");
+//        Scanner scanner = new Scanner(System.in);
+//
+//        int nubmer = (int) (Math.random() * 10);
+//        boolean number2 = true;
+//        while (number2) {
+//            System.out.println("Введите число");
+//            int number1 = scanner.nextInt();
+//            if (nubmer == number1) {
+//                number2 = false;
+//                System.out.println("Вы выйграли");
+//
+//
+//            } else {
+//                System.out.println("Попробуйте еще раз");
+//            }
+//
+//
+//        }
+//    }
+    public void nechet(){
+        Scanner scanner = new Scanner(su)
+    }
+}
+
+
+
+
+
+
+
+
+
---
+++ Main.java
@@ -0,0 +1,18 @@
+package Lessons_2;
+import java.util.Scanner;
+import java.util.Random;
+public class Main {
+    public static void main(String[] args){
+        Culkulet culkulet = new Culkulet();
+
+//        culkulet.fibo();
+//        culkulet.tabl();
+//        culkulet.elka1();
+//        culkulet.elka2();
+//        culkulet.vremya(3);
+//        culkulet.bilet();
+
+
+    }
+
+}
